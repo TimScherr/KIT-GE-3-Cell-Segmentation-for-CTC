ae245da84ced2cd31c088494e972551ef19de6a5
Tim Scherr <tim.scherr@kit.edu>
2021-09-30 19:12:37 +0200

    script bugfix

--- eval_kit-sch-ge.sh
+++ eval_kit-sch-ge.sh
@@ -6,7 +6,7 @@ conda activate kit-sch-ge-2021-cell_segmentation_ve
 
 # Evaluation of the specialized models trained on GTs+STs from the reproduced kit-sch-ge training dataset (for multiple thresholds, the option --fuse_z_seeds has not been applied in evaluation but in inference for some 3D cell types)
 python ./eval.py --cell_type "BF-C2DL-HSC" --mode "GT" --model "BF-C2DL-HSC_GT+ST_kit-sch-ge" --batch_size 16 --artifact_correction --th_cell 0.07 --th_seed 0.35 0.45
-python ./cell_eval.py --cell_type "BF-C2DL-MuSC" --mode "GT" --model "BF-C2DL-MuSC_GT+ST_kit-sch-ge" --batch_size 8 --artifact_correction --th_cell 0.07 --th_seed 0.35 0.45
+python ./eval.py --cell_type "BF-C2DL-MuSC" --mode "GT" --model "BF-C2DL-MuSC_GT+ST_kit-sch-ge" --batch_size 8 --artifact_correction --th_cell 0.07 --th_seed 0.35 0.45
 python ./eval.py --cell_type "DIC-C2DH-HeLa" --mode "GT" --model "DIC-C2DH-HeLa_GT+ST_kit-sch-ge" --batch_size 16 --th_cell 0.07 --th_seed 0.35 0.45
 python ./eval.py --cell_type "Fluo-C2DL-MSC" --mode "GT" --model "Fluo-C2DL-MSC_GT+ST_kit-sch-ge" --batch_size 16 --th_cell 0.07 --th_seed 0.35 0.45
 python ./eval.py --cell_type "Fluo-C3DH-A549" --mode "GT" --model "Fluo-C3DH-A549_GT+ST_kit-sch-ge" --batch_size 8 --th_cell 0.07 --th_seed 0.35 0.45
